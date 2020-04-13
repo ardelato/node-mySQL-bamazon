394f62fd6a2021df60ac717b12257c1c305d1b92
Angel de la Torre <angel.dlt.1996@gmail.com>
2020-04-12 20:11:49 -0700

    Got a list of current product ids to show for inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+var product_choices = [];
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -13,9 +14,30 @@ connection.connect(function (err) {
     "SELECT item_id,product_name,price FROM products WHERE stock_quantity <> 0",
     function (err, data) {
       if (err) throw err;
-      var products = [];
       console.table(data);
+      data.forEach((row) => {
+        product_choices.push(row.item_id);
+      });
+      start();
     }
   );
   connection.end();
 });
+
+function start() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Choose the product you would like to buy: ",
+        choices: product_choices,
+        name: "id_choice",
+      },
+    ])
+    .then(function (inquirerResponse) {
+      console.log(inquirerResponse);
+    })
+    .catch(function (err) {
+      throw err;
+    });
+}
